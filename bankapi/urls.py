68d1433ee070a27985a930f9e947951d0139c5c4
Galvon0256 <tanmay.madambi@gmail.com>
2025-08-14 22:17:13 +0530

    deployed

--- bankapi/urls.py
+++ bankapi/urls.py
@@ -4,6 +4,7 @@ from branches import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.BankList.as_view(), name='bank-list'),
     path('banks/', views.BankList.as_view(), name='bank-list'),
     path('banks/<int:bank_id>/branches/', views.BankBranchesList.as_view(), name='bank-branches'),
     path('branches/<str:ifsc>/', views.BranchDetail.as_view(), name='branch-detail'),
